4254f4e8b993597e7ecadfcdea5e1cfcebc6a0f9
18310799137 <18310799137@163.com>
2018-01-26 11:14:02 +0800

    添加JSON 依赖，org.sf.json-lib，jersey客户端post访问rest服务方式

--- src/main/java/com/zgh/springboot/rest/control/RestControl.java
+++ src/main/java/com/zgh/springboot/rest/control/RestControl.java
@@ -2,7 +2,9 @@ package com.zgh.springboot.rest.control;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -25,18 +27,40 @@ public class RestControl {
 		user.setAge(10);
 		user.setId(5);
 		user.setName("风清扬");
-		//浏览器访问 http://localhost:9001/rest/getUserInfo  返回{"id":5,"name":"风清扬","age":10}
-		//通过浏览器 访问 http://localhost:9001/rest/application.wadl 可以查看rest接口详细信息
-	
+		// 浏览器访问 http://localhost:9001/rest/getUserInfo 返回{"id":5,"name":"风清扬","age":10}
+		// 通过浏览器 访问 http://localhost:9001/rest/application.wadl 可以查看rest接口详细信息
+
 		return user;
 	}
 
-	@GET
+	/**
+	 * @description:客户端传递json到服务端 封装为对象
+	 * @author FreeGuardian
+	 * @param user
+	 * @return
+	 */
+	@POST
 	@Path("/saveUserInfo")
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response saveUserInfo(User user) {
-		String result = "User saved : " + user;
-		return Response.status(201).entity(result).build();
+		logger.debug("RequestMapping : [ rest/saveUserInfo ]  run");
+		String result = "server receive code : " + user;
+		return Response.status(200).entity(result).build();
+	}
+
+	/**
+	 * @description:使用路径直接作为参数使用,访问 http://localhost:9001/rest/path/*
+	 *                              都将访问本方法,并且*将作为userId的值传递给方法
+	 * @author FreeGuardian
+	 * @param userId
+	 * @return
+	 */
+	@POST
+	@Path("/path/{id}")
+	public String pathParam(@PathParam("id") String userId) {
+		logger.debug("RequestMapping : [ rest/path/{id} ]  run");
+
+		return "Server recieve param userId : " + userId;
 	}
 
 }
--- src/test/java/com/zgh/springboot/test/SpringBootApplicationTest.java
+++ src/test/java/com/zgh/springboot/test/SpringBootApplicationTest.java
@@ -2,29 +2,78 @@ package com.zgh.springboot.test;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import net.sf.json.JSONObject;
+
 public class SpringBootApplicationTest {
+	static Client client = ClientBuilder.newClient();
+
 	/**
 	 * @description:接口调用
 	 * @author FreeGuardian
-	 * @param args
 	 */
-	public static void main(String[] args) {
-
-		Client client = ClientBuilder.newClient();
+	public static void requestRest() {
 
 		WebTarget webTarget = client.target("http://localhost:9001/").path("bootRest/queryUser");
 
-		Response retn = webTarget.request().get();
+		Response response = webTarget.request().get();
 
-		int retnStatus = retn.getStatus();
+		int retnStatus = response.getStatus();
 
-		String retnCode = retn.readEntity(String.class).toString();
+		String retnCode = response.readEntity(String.class).toString();
 
 		System.out.println("retnStatus:" + retnStatus);
 
 		System.out.println("retnCode:" + retnCode);
 	}
+
+	public static void postParamToRestService() {
+		WebTarget webTarget = client.target("http://localhost:9001/").path("rest/saveUserInfo");
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.element("id", "9527");
+		jsonObject.element("age", "900");
+		jsonObject.element("name", "独孤求败");
+
+		Response response = webTarget.request(MediaType.APPLICATION_JSON_TYPE).post(Entity.json(jsonObject));
+		String retnStatusInfo = response.getStatusInfo().toString();
+
+		String retnCode = response.readEntity(String.class);
+
+		String retnHeader = response.getStringHeaders().toString();
+
+		System.out.println("retnStatusInfo:" + retnStatusInfo + "\tretnStatus:" + response.getStatus());
+
+		System.out.println("retnHeader:" + retnHeader);
+
+		System.out.println("retnCode:" + retnCode + "\tretnCodeLength:" + retnCode.length());
+	}
+
+	public static void requestPathParamService() {
+		WebTarget webTarget = client.target("http://localhost:9001/").path("rest/path/A00001");
+
+		Response response = webTarget.request().post(Entity.html(null));
+
+		int retnStatus = response.getStatus();
+
+		String retnCode = response.readEntity(String.class);
+
+		System.out.println("retnStatus:" + retnStatus);
+
+		System.out.println("retnCode:" + retnCode);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("==============================================================");
+		requestRest();
+		System.out.println("==============================================================");
+		postParamToRestService();
+		System.out.println("==============================================================");
+		requestPathParamService();
+		System.out.println("==============================================================");
+	}
+
 }
