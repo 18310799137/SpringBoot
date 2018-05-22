365307dcf7a3c71cb903e98ddd7f9daeb9349326
18310799137 <18310799137@163.com>
2018-05-22 16:58:20 +0800

    删除MQ 工具类

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -1,7 +1,11 @@
+--创建数据库名称 mydatabase 设置编码为utf-8
+create database mydatabase character set utf8;
+-- 切换到数据库 mydatabase  
+use mydatabase;
+-- 如果存在表 t_user 则删除
 drop table  if exists t_user;
-
+-- 创建表 t_user
 create table t_user(id int,name varchar(20),age int);
-
+-- 在表t_user中添加两条数据
 insert into t_user values(1,'zhangsan',30);
-
 insert into t_user values(2,'李四',30); 
